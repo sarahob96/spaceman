f6133430bc6222ba9b212d432be7dd968db65b47
sarahob96 <sarahobrien15@gmail.com>
2022-03-10 10:41:42 +0000

    define a game intro function where the user is welcomed and asked to input a username

--- run.py
+++ run.py
@@ -2,3 +2,16 @@ from pyfiglet import figlet_format
 from words import space_words
 
 print(figlet_format("Welcome to Spaceman", font = "small"))
+
+def game_intro():
+    """ 
+    Welcomes user and asks them for a username
+    """
+    print("Welcome to Spaceman, the word guessing game: Space edition")
+    print("Please enter a username")
+
+    user_name = input()
+    print(f"Welcome {user_name}, time to play Spaceman!")
+
+
+game_intro()
