9eb517d56388e66f33dfe144745ac4802909dc47
sarahob96 <sarahobrien15@gmail.com>
2022-03-17 16:05:39 +0000

    import colorama and use it to add colours to text throughout the game process

--- run.py
+++ run.py
@@ -2,15 +2,23 @@ from pyfiglet import figlet_format
 from words import space_words
 import random
 import os
+from colorama import init, Fore, Back, Style
+
+init()
 
 
+def reset_screen():
+    os.system('reset')
+
+def game_heading():
+    print(Fore.CYAN + figlet_format("Spaceman", font = "small")) 
 
 def game_intro():
     """ 
     Welcomes user
     """
     game_heading()
-    print("Welcome to Spaceman, the word guessing game: Space edition")
+    print(Fore.YELLOW + "Welcome to Spaceman, the word guessing game: Space edition")
 
     
     menu()
@@ -21,7 +29,7 @@ def menu():
 
     
     
-    print("\nMENU")
+    print(Fore.WHITE +"\n     MENU    ")
     print("1: Instructions")
     print("2: Play game \n")
     user_input = input("Please enter 1 or 2:  \n")
@@ -40,7 +48,7 @@ def instructions():
    
     reset_screen()
     game_heading()
-    print(" \nSPACEMAN")
+    print(Fore.YELLOW + " \nSPACEMAN \n")
     print("The aim of the game is to avoid the aliens by guessing the correct word before your chances run out ")
     print("1) You will have 6 chances to guess the right answer")
     print("2) All words are space related ")
@@ -48,24 +56,20 @@ def instructions():
     print("4) You will be given the chance to restart the game at the end of each game ")
     print("5) Goodluck \n")
     
-    users_input = input("Press 1 to play the game:  ")
+    users_input = input(Fore.CYAN + "Press 1 to play the game:  ")
     if users_input == "1":
         reset_screen()
         user_name()
     
- 
-    
-    
-
 
 def user_name():
     
     game_heading()
-    print("Please enter a username \n")
+    print(Fore.WHITE + "Please enter a username \n")
     global users_name
     users_name = input().upper()
 
-    print(f" \nWelcome {users_name}, time to play Spaceman!")
+    print(Fore.LIGHTMAGENTA_EX + f" \nWelcome {users_name}, time to play Spaceman!")
     
     game()
 
@@ -93,7 +97,7 @@ def game():
 
     while chances_remaining > 0:
         
-        print(illustrations(chances_remaining))
+        print(Fore.YELLOW + illustrations(chances_remaining))
         correct_word = ""
 
         for letter in word: 
@@ -106,38 +110,39 @@ def game():
         print(correct_word)
         
 
-        print(f"You have {chances_remaining} chances left \n")
+        print(Fore.LIGHTBLUE_EX+ f"\nYou have {chances_remaining} chances left \n")
 
         if len(letters_guessed) > 0:
-         print(f"you have guessed these letters {letters_guessed} \n")
+         print(Fore.LIGHTMAGENTA_EX + f"you have guessed these letters: {letters_guessed} \n")
 
-        enter_letter = input("Please enter a letter : \n")
+        enter_letter = input(Fore.WHITE + "Please enter a letter : \n")
     
 
         
 
         if enter_letter in letters_guessed:
-             print(f"You have already guessed {enter_letter}\n")
+             print(Fore.RED + f"You have already guessed {enter_letter}\n")
         elif enter_letter in alphabet:
              letters_guessed.append(enter_letter)  
              if enter_letter not in letters:
-                 print(f"\nSorry, '{enter_letter}' is not in the word")
+                 print(Fore.RED + f"\nSorry, '{enter_letter}' is not in the word")
                  chances_remaining = chances_remaining - 1
                  
         elif len(enter_letter) > 1:
-            print("please enter one letter at a time\n")
+            print(Fore.RED + "please enter one letter at a time\n")
         else:
-            print("please enter a valid letter\n")
+            print(Fore.RED + "please enter a valid letter\n")
         
 
         if enter_letter in letters:
-            print(f"You guessed a correct letter: '{enter_letter}'\n ")
+            print(Fore.GREEN + f"You guessed a correct letter: '{enter_letter}'\n ")
             letters.remove(enter_letter)
         
 
         if len(letters) == 0:
+            game_heading()
             print(f"Congratulations, you have guessed the word: {word}")
-            print("You escaped the aliens\n")
+            print("You have escaped the aliens\n")
             exit()
 
     print(illustrations(chances_remaining))
@@ -148,27 +153,34 @@ def game():
 
 def end_of_game():
         
-        print("OH NO.. you have 0 chances left")
+        print(Fore.YELLOW + "OH NO.. you have 0 chances left")
         print("You have been caught by the ALIENS \n")
-        print(f"The correct word was: {word}\n ")
-        print("Would you like to restart the game?\n")
+        print(Fore.LIGHTMAGENTA_EX+ f"The correct word was: {word}\n ")
+        print(Fore.WHITE + "Would you like to restart the game?\n")
         answer = input("Please enter yes or no:  \n")
         if answer == "yes":
           reset_screen()
-          game_intro()
+          user_name()
         elif answer == "no":
           reset_screen()
+          game_heading()
           exit()
         else:
-          print("Please enter a valid answer\n")
+          print(Fore.RED + "Please enter a valid answer\n")
 
         
      
 
 def exit():
-    game_heading()
-    print(f"Thanks {users_name} for playing Spaceman \n")
-
+    
+    print(Fore.CYAN + f"Thanks {users_name} for playing Spaceman \n")
+    exit_decision = input("\n Would you like another go at escaping the aliens? Enter yes or no: ")
+    if exit_decision == "yes":
+        reset_screen()
+        user_name()
+    elif answer == "no":
+        reset_screen()
+        print("We hope to see you back playing Spaceman soon!")
     
 
     
@@ -273,11 +285,7 @@ def illustrations(chances_remaining):
     return spaceman[chances_remaining]
     
 
-def reset_screen():
-    os.system('reset')
-
-def game_heading():
-    print(figlet_format("Spaceman", font = "small"))   
+  
 
 def functions ():
    
